3a0e279dcafd17129660f91c23c1754eac5c3b07
Winston Pacheco Junior <winston@atleastonce.cc>
2021-04-11 09:26:16 -0300

    revisando como o layout eh montado para facilitar a troca de cores caso seja necessario

--- calc.py
+++ calc.py
@@ -2,30 +2,38 @@ import PySimpleGUI as sg
 
 sg.theme('Dark Grey 13')
 
-linha1 = [sg.Button('AC', size=(3, 2), key='-AC-', pad=(0,0), button_color=('white', '#585b5e'), font=('Helvetica', 12)), 
-          sg.Button('+/-', size=(3, 2), key='-POSNEG-', pad=(0,0), button_color=('white', '#585b5e'), font=('Helvetica', 12)), 
-          sg.Button('%', size=(3, 2), key='%', pad=(0,0), button_color=('white', '#585b5e'), font=('Helvetica', 12)), 
-          sg.Button('/', size=(3, 2), key='/', pad=(0,0), button_color=('white', '#bf780d'), font=('Helvetica', 12))]
+""" Configuração de formato e cores dos botões """
+fb = { 'size':(3, 2), 'pad':(0,0), 'button_color':('white', '#585b5e'), 'font':('Helvetica', 12) }
+op = { 'size':(3, 2), 'pad':(0,0), 'button_color':('white', '#bf780d'), 'font':('Helvetica', 12) }
+nm = { 'size':(3, 2), 'pad':(0,0), 'button_color':('white', '#888b8f'), 'font':('Helvetica', 12) }
+lb = { 'size':(11, 2), 'pad':(0,0), 'button_color':('white', '#888b8f'), 'font':('Helvetica', 12) }
 
-linha2 = [sg.Button('7', size=(3, 2), key='7', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('8', size=(3, 2), key='8', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('9', size=(3, 2), key='9', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('*', size=(3, 2), key='*', pad=(0,0), button_color=('white', '#bf780d'), font=('Helvetica', 12))]
+""" Linhas de botões da calculadora com suas devidas formatações """
+linha1 = [sg.Button('AC', key='-AC-', **fb), 
+          sg.Button('+/-', key='-POSNEG-', **fb), 
+          sg.Button('%', key='%', **fb), 
+          sg.Button('/', key='/', **op)]
 
-linha3 = [sg.Button('4', size=(3, 2), key='4', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('5', size=(3, 2), key='5', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('6', size=(3, 2), key='6', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('-', size=(3, 2), key='-', pad=(0,0), button_color=('white', '#bf780d'), font=('Helvetica', 12))]
+linha2 = [sg.Button('7', key='7', **nm), 
+          sg.Button('8', key='8', **nm), 
+          sg.Button('9', key='9', **nm), 
+          sg.Button('*', key='*', **op)]
 
-linha4 = [sg.Button('1', size=(3, 2), key='1', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('2', size=(3, 2), key='2', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('3', size=(3, 2), key='3', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('+', size=(3, 2), key='+', pad=(0,0), button_color=('white', '#bf780d'), font=('Helvetica', 12))]
+linha3 = [sg.Button('4', key='4', **nm), 
+          sg.Button('5', key='5', **nm), 
+          sg.Button('6', key='6', **nm), 
+          sg.Button('-', key='-', **op)]
 
-linha5 = [sg.Button('0', size=(11, 2), key='0', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button(',', size=(3, 2), key=',', pad=(0,0), button_color=('white', '#888b8f'), font=('Helvetica', 12)), 
-          sg.Button('=', size=(3, 2), key='=', pad=(0,0), button_color=('white', '#bf780d'), font=('Helvetica', 12))]
+linha4 = [sg.Button('1', key='1', **nm), 
+          sg.Button('2', key='2', **nm), 
+          sg.Button('3', key='3', **nm), 
+          sg.Button('+', key='+', **op)]
 
+linha5 = [sg.Button('0', key='0', **lb), 
+          sg.Button('.', key='.', **nm), 
+          sg.Button('=', key='=', **op)]
+
+""" Organização das linhas em um layout (formato de exibição ordem dos componentes de forma integrada) """
 layout = [
      [sg.Text('0', size=(13,1), pad=(0,0), key='-RESULT-', text_color='white', background_color='#414345', font=('Helvetica', 30), justification='right')],
      linha1,
@@ -35,15 +43,16 @@ layout = [
      linha5
 ]
 
-# layout =  [[sg.Button('?', size=(4, 2), key=(i,j), pad=(0,0)) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
-
-
+""" Cria a janela em si usando o layout que foi criado acima """
 window = sg.Window('Calculadora', layout)
+
+view_model = { '' }
 
 while True:  # Event Loop
     event, values = window.read()
     print(event, values)
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    # já que não aprendi ainda como evitar o "while True" a condição de saída é a primeira opção do meu loop
+    if event == sg.WIN_CLOSED:
         break
     if event == '-UM-':
         window['-RESULT-'].update()
